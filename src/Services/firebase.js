f7583d2b1eb78d7de3d7c92fc86177966e16834e
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-06-08 20:27:52 +0300

    Making card component as a wraper component

---
+++ src/Services/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmKWvhyOozwWalhzILqebHXU6C8CGmSG0",
+  authDomain: "expenses-6f3a1.firebaseapp.com",
+  projectId: "expenses-6f3a1",
+  storageBucket: "expenses-6f3a1.appspot.com",
+  messagingSenderId: "718334636215",
+  appId: "1:718334636215:web:b5ba44cd29de352bdd5d32",
+  measurementId: "G-28C4TFMGTJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(firebaseApp);
