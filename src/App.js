fa5eaaff0a28e4b55649644802c0e8f054659160
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2023-06-09 21:02:58 +0300

    making a firebase.js file and installing firebse in the project to be used by users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ExpenseForm from '../src/components/Expenses/ExpenseForm/ExpenseForm.jsx';
 import AllExpeseItems from './components/Expenses/AllExpeseItems/AllExpeseItems.jsx';
 import { expenses } from './assets/expenses.js';
+import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar/>
       <ExpenseForm/>
       <AllExpeseItems expenses={expenses} />
     </div>
